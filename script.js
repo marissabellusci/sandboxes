b6f2abc7277f6e5d61a0a6cd130ec38abcc4fc69
Marissa <marissabellusci@gmail.com>
2020-11-08 12:32:48 -0700

    updated 'book' object

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function Book(title, author, pages, read){
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
+        return `${title} by ${author}, ${pages} pages. ${read}`
     }
 
-    
+
 }
