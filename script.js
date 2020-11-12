fed5e366208e24805421e87849cf4e9168ddd755
Marissa <marissabellusci@gmail.com>
2020-11-11 17:16:11 -0700

    set up functionality

--- script.js
+++ script.js
@@ -29,16 +29,26 @@ function  addBookToLibrary(newBook){
 /* Function to display books on page */
 
 function display(){
-    console.log(myLibrary)
+    console.log(myLibrary);
+    let para = null;
+    let node = null;
+    
 
     for (i = 0; i < myLibrary.length; i++){
-        console.log(myLibrary[i]);
-        const para = document.createElement("p");
-        const node = document.createTextNode(myLibrary[i].info().toString());
+        //console.log(myLibrary[i]);
+
+        if (!para && !node){
+            para = document.createElement("p");
+            node = document.createTextNode(myLibrary[i].info().toString());
         para.appendChild(node);
 
         const element = document.getElementById("library");
         element.appendChild(para);
+        }
+
+        else {
+            node.nodeValue = myLibrary[i].info().toString();
+        }
     }
     
 };
@@ -55,3 +65,85 @@ const newBookElement = document.getElementById("container");
 newBookElement.appendChild(newBookButton);
 
 /*Next I need to make the newBookButton create a form to add a new book*/
+
+
+const form = document.createElement("form");    
+const title = document.createElement("input");
+    title.setAttribute("type","text");
+    title.setAttribute("name", "title");
+    title.setAttribute("placeholder", "Title");
+
+const author = document.createElement("input");
+    author.setAttribute("type","text");
+    author.setAttribute("name","author");
+    author.setAttribute("placeholder", "Author");
+
+const pages = document.createElement("input");
+    pages.setAttribute("type","number");
+    pages.setAttribute("name","pages");
+    pages.setAttribute("placeholder","Number of Pages");
+
+const readOrNot = document.createElement("input");
+    readOrNot.setAttribute("type", "checkbox");
+    readOrNot.setAttribute("name", "readOrNot");
+
+
+    const readLabel = document.createElement("label");
+        const readLabelTextContent = document.createTextNode("I've read this");
+        readLabel.appendChild(readLabelTextContent);
+
+    readOrNot.appendChild(readLabel);
+
+const submit = document.createElement("button");
+const submitText = document.createTextNode('Add');
+submit.appendChild(submitText);
+
+form.appendChild(title);
+form.appendChild(author);
+form.appendChild(pages);
+form.appendChild(readOrNot);
+form.appendChild(readLabel);
+form.appendChild(submit);
+
+const formElement = document.getElementById("form-pop-up");
+newBookButton.addEventListener("click",function() {formElement.appendChild(form)});
+
+function titleCase(str){
+    str = str.toLowerCase().split(" ").map(function(word) {
+        return (word.charAt(0).toUpperCase() + word.slice(1))
+    });
+    
+    return str.join(' ');
+};
+
+function handleSubmit(e){
+   
+    e.preventDefault();
+
+    if (!title.value  || !author.value || !pages.value || !readOrNot.value){
+        return;
+    }
+
+    if (readOrNot.checked == true) {
+        readStatus = "I've read it.";
+    }
+
+    if (readOrNot.checked == false){
+        readStatus = "Haven't read this one yet.";
+    }
+
+    const creatingBook = new Book (titleCase(title.value), titleCase(author.value), pages.value, readStatus)
+    addBookToLibrary(creatingBook);
+    display();
+    console.log("submit");
+    formElement.removeChild(form);
+    title.value = null;
+    author.value = null;
+    pages.value = null;
+    readOrNot.checked = false;
+}
+submit.addEventListener("click", handleSubmit);
+
+window.addEventListener("load",function(){    
+    display();
+    });
