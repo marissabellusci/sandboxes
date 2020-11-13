1d42281c9268caa413eb91d495bcf8160183a180
Marissa <marissabellusci@gmail.com>
2020-11-13 11:57:06 -0700

    defined Book object prototype

--- script.js
+++ script.js
@@ -12,25 +12,30 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        if (this.read == true) {
-            return `${this.title} by ${this.author}, ${this.pages} pages. I've read it.`
-            }
-    
-            else return `${this.title} by ${this.author}, ${this.pages} pages. I haven't read it.`
-    }
-};
+}
+
 
 // This is the prototype//
+
+Book.prototype.info = function() {   
+    if (this.read == true) {
+    return `${this.title} by ${this.author} (${this.pages} pages) I've read it.`
+    }
+
+    else return `${this.title} by ${this.author} (${this.pages} pages) I haven't read it.`
+
+}
+
+//newBook.proptotype = Object.create(Book.prototype);
 
 Book.prototype.beenReadToggle = function (){
     this.read = !this.read;
     this.info = function() {
         if (this.read == true) {
-        return `${this.title} by ${this.author}, ${this.pages} pages. I've read it.`
+        return `${this.title} by ${this.author} (${this.pages} pages)  I've read it.`
         }
 
-        else return `${this.title} by ${this.author}, ${this.pages} pages. I haven't read it.`
+        else return `${this.title} by ${this.author} (${this.pages} pages). I haven't read it.`
 
     }
     const bookIndex = myLibrary.indexOf(this);
@@ -74,8 +79,8 @@ function display(){
         if (!para && !node){
             para = document.createElement("p");
             node = document.createTextNode(myLibrary[i].info().toString());
-            para.setAttribute("name",title.value);
-            para.setAttribute("id",title.value);
+            para.setAttribute("name",titleField.value);
+            para.setAttribute("id",titleField.value);
             para.setAttribute("data-index-number", itemIndex);
             button = document.createElement("button");
             button.setAttribute("id","remove");
@@ -99,7 +104,7 @@ function display(){
             readButton.addEventListener("click",function(e){
                 console.log(this.parentElement.dataset.indexNumber);
               
-                myLibrary[this.parentElement.dataset.indexNumber].beenReadToggle();
+                myLibrary[this.parentElement.dataset.indexNumber].beenReadToggle(Book);
 
 
                 if (e.target.classList.contains("read")){
@@ -158,20 +163,20 @@ newBookElement.appendChild(newBookButton);
 
 
 const form = document.createElement("form");    
-const title = document.createElement("input");
-    title.setAttribute("type","text");
-    title.setAttribute("name", "title");
-    title.setAttribute("placeholder", "Title");
+const titleField = document.createElement("input");
+    titleField.setAttribute("type","text");
+    titleField.setAttribute("name", "title");
+    titleField.setAttribute("placeholder", "Title");
 
-const author = document.createElement("input");
-    author.setAttribute("type","text");
-    author.setAttribute("name","author");
-    author.setAttribute("placeholder", "Author");
+const authorField = document.createElement("input");
+    authorField.setAttribute("type","text");
+    authorField.setAttribute("name","author");
+    authorField.setAttribute("placeholder", "Author");
 
-const pages = document.createElement("input");
-    pages.setAttribute("type","number");
-    pages.setAttribute("name","pages");
-    pages.setAttribute("placeholder","Number of Pages");
+const pagesField = document.createElement("input");
+    pagesField.setAttribute("type","number");
+    pagesField.setAttribute("name","pages");
+    pagesField.setAttribute("placeholder","Number of Pages");
 
 const readOrNot = document.createElement("input");
     readOrNot.setAttribute("type", "checkbox");
@@ -188,9 +193,9 @@ const submit = document.createElement("button");
 const submitText = document.createTextNode('Add');
 submit.appendChild(submitText);
 
-form.appendChild(title);
-form.appendChild(author);
-form.appendChild(pages);
+form.appendChild(titleField);
+form.appendChild(authorField);
+form.appendChild(pagesField);
 form.appendChild(readOrNot);
 form.appendChild(readLabel);
 form.appendChild(submit);
@@ -239,7 +244,7 @@ function handleSubmit(e){
    
     e.preventDefault();
 
-    if (!title.value  || !author.value || !pages.value || !readOrNot.value){
+    if (!titleField.value  || !authorField.value || !pagesField.value || !readOrNot.value){
         return;
     }
 
@@ -253,15 +258,15 @@ function handleSubmit(e){
     
     }
 
-    const creatingBook = new Book (titleCase(title.value), titleCase(author.value), pages.value, readStatus)
+    const creatingBook = new Book (titleCase(titleField.value), titleCase(authorField.value), pagesField.value, readStatus)
     addBookToLibrary(creatingBook);
     console.log(myLibrary.indexOf(creatingBook));
     itemIndex = myLibrary.indexOf(creatingBook);
     display();
     formElement.removeChild(form);
-    title.value = null;
-    author.value = null;
-    pages.value = null;
+    titleField.value = null;
+    authorField.value = null;
+    pagesField.value = null;
     readOrNot.checked = false;
     let removeButtons = document.querySelectorAll(".remove");
     removeButtons.forEach(removeButton => removeButton.addEventListener("click",handleRemove));
@@ -272,12 +277,8 @@ window.addEventListener("load",function(){
     display();
     });
 
-    let removerButtons = document.querySelectorAll(".remove");
-    removerButtons.forEach(removeButton => removeButton.addEventListener("click",handleRemove));
-
-
-
-
+let removerButtons = document.querySelectorAll(".remove");
+removerButtons.forEach(removeButton => removeButton.addEventListener("click",handleRemove));
 
 
 
